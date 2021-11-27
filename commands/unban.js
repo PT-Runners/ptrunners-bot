24b7047aa001c729761c37ca8c8d6abad2fa19d1
DCard <diogocardoso67@tecnico.ulisboa.pt>
2021-11-27 12:15:42 +0000

    Add unban command

---
+++ commands/unban.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { ptrImage, ticketsChannel } = require('./commands_config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unban')
+		.setDescription('Mostra informação sobre pedidos de unban'),
+	async execute(interaction) {
+        const response = {
+			components: [],
+			embeds: [
+				new MessageEmbed()
+					.setColor('RANDOM')
+					.setAuthor('​', ptrImage, 'https://ptrunners.net')
+					.setTitle('Pedido de unban')
+					.setDescription(
+                        `Não vendemos unbans, podes apenas pedir desculpa pelo erro cometido e pedir que sejas desbanido através de um ticket. 
+						Dirige-te à sala <#${ticketsChannel}>`,
+                    ),
+			],
+            ephemeral: true,
+		};
+		interaction.reply(response);
+	},
+};
