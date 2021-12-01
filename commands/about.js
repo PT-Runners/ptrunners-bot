f27f0493e5da728017c8f1431b6ca4850a1a0ad6
DCard <diogocardoso67@tecnico.ulisboa.pt>
2021-12-01 22:52:10 +0000

    Update version

--- commands/about.js
+++ commands/about.js
@@ -25,7 +25,7 @@ module.exports = {
 					.setTitle('PTRunners Discord Bot')
 					.setDescription(
                         `Made with :heart: by <@351410153523904513>
-                        version 1.2.1`,
+                        version 1.2.2`,
                     )
 					.setFooter(`Uptime: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds `),
 			],
