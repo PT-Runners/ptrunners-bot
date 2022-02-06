0886774a008d9e44683b989e5bbbc60bb0e771c6
D-Card <D-Card@users.noreply.github.com>
2022-02-06 21:46:55 +0000

    Add webhooks (3/5)

--- http_requests/http.js
+++ http_requests/http.js
@@ -1,5 +1,67 @@
-module.exports.create_gang = function(client, req){
-        client.channels.fetch('909779420619866132').then(channel => {
-            channel.send('It worked again!');
-        })
-      }
+const { guildId } = require('../config.json');
+const { staffRole, gangsCategory } = require('../commands/commands_config.json');
+const { Permissions } = require('discord.js');
+
+module.exports.create_gang = function (client, gang_name) {
+  client.guilds.fetch(guildId).then(guild => {
+    guild.roles.create({
+      name: gang_name,
+      color: 'RANDOM',
+      mentionable: true,
+    }).then(guildRole => {
+      guild.channels.create(`${gang_name}-text`, {
+        parent: gangsCategory,
+        type: 'GUILD_TEXT',
+        permissionOverwrites: [
+          {
+            id: guild.roles.resolve(guild.roles.everyone.id),
+            deny: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: guildRole.id,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: guild.roles.resolve(staffRole),
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+        ],
+        topic: `Sala de texto da gang ${gang_name}`,
+      });
+      guild.channels.create(`${gang_name}-voice`, {
+        parent: gangsCategory,
+        type: 'GUILD_VOICE',
+        permissionOverwrites: [
+          {
+            id: guild.roles.resolve(guild.roles.everyone.id),
+            deny: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: guildRole.id,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+          {
+            id: guild.roles.resolve(staffRole),
+            allow: [Permissions.FLAGS.VIEW_CHANNEL],
+          },
+        ]
+      });
+    });
+  })
+}
+
+module.exports.add_to_gang = function (client, player, gang) {
+  client.guilds.fetch(guildId).then(guild => {
+    guild.members.fetch(player).then(player => {
+      player.roles.add(guild.roles.cache.find(role => role.name == gang));
+    });
+  })
+}
+
+module.exports.delete_gang = function (client, gang) {
+  client.guilds.fetch(guildId).then(guild => {
+    guild.roles.cache.find(role => role.name == gang).delete();
+    guild.channels.cache.find(channel => channel.name == `${gang.toLowerCase().replaceAll(" ", "-")}-text`).delete();
+    guild.channels.cache.find(channel => channel.name == `${gang}-voice`).delete();
+  });
+}
--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const http = require('./http_requests/http.js');
 const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = 3000;
 const { Client, Collection, Intents } = require('discord.js');
-const { token, game } = require('./config.json');
+const { token, game, webhook_token } = require('./config.json');
 const { ticketsChannel, suggestionsChannel, verificationChannel } = require('./commands/commands_config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -39,25 +40,91 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.on('messageCreate', msg => {
-    if ((msg.channel == ticketsChannel || msg.channel == suggestionsChannel) && msg.author.bot == false) {
+	if ((msg.channel == ticketsChannel || msg.channel == suggestionsChannel) && msg.author.bot == false) {
 		msg.delete();
 	}
 	if (msg.channel == verificationChannel) {
-		setTimeout(function() {
+		setTimeout(function () {
 			msg.delete().catch(error => {
-				console.error(error);
-				});
+				console.log("Error deleting message.");
+			});
 		}, 1000 * 30);
 	}
 });
 
 client.login(token);
 
-app.get('/create-gang', (req, res) => {
-	res.send('Creating gang role!');
-	http.create_gang(client, req);
-  });
+//FIXME:
+app.use('/webhooks', (req, res, next) => {
+	var webhookToken = req.body.token;
+	if (webhookToken && webhookToken == webhook_token) {
+		next();
+	}
+	else res.redirect('/error');
+});
 
-  app.listen(port, () => {
+app.get('/error', (req, res) => {
+	res.send('Wrong request');
+});
+
+app.post('/webhooks/create-gang', (req, res) => {
+	var gang_name = req.body.name;
+	if (!gang_name) {
+		res.redirect('/error');
+	}
+	else {
+		http.create_gang(client, gang_name);
+		res.send('Creating gang role!');
+	}
+});
+
+app.post('/webhooks/delete-gang', (req, res) => {
+	var gang_name = req.body.name;
+	if (!gang_name) {
+		res.redirect('/error');
+	}
+	else {
+		http.delete_gang(client, gang_name);
+		res.send('Deleting gang role!');
+	}
+});
+
+app.post('/webhooks/add-to-gang', (req, res) => {
+	var gang_name = req.body.name;
+	var player = req.body.player;
+	if (!gang_name || !player) {
+		res.redirect('/error');
+	}
+	else {
+		http.add_to_gang(client, player, gang_name);
+		res.send('Adding player to gang!');
+	}
+});
+
+// app.post('/webhooks/remove-from-gang', (req, res) => {
+// 	var gang_name = req.body.name;
+// 	var player = req.body.player;
+// 	if (!gang_name || !player) {
+// 		res.redirect('/error');
+// 	}
+// 	else {
+// 		res.send('Removing player from gang!');
+// 		http.remove_from_gang(client, player, gang_name);
+// 	}
+// });
+
+// app.post('/webhooks/rename-gang', (req, res) => {
+// 	var gang_name = req.body.name;
+// 	var new_name = req.body.new-name;
+// 	if (!gang_name || !new_name) {
+// 		res.redirect('/error');
+// 	}
+// 	else {
+// 		res.send('Renaming gang!');
+// 		http.rename_gang(client, gang_name, new_name);
+// 	}
+// });
+
+app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
-  });
+});
